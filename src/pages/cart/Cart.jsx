daa1279473dc2e89d74c93f7801d122e6de6736c
sai lailitha <sai@gmail.com>
2024-03-01 23:24:20 +0530

    sEtup the code

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -92,7 +92,7 @@ function Cart() {
       amount: parseInt(grandTotal * 100),
       currency: "INR",
       order_receipt: 'order_rcptid_' + name,
-      name: "Blastt",
+      name: "Shopify",
       description: "for testing purpose",
       handler: function (response) {
         console.log(response)
